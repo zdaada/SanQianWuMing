966dbf7f70e50c5a265cf25e5f18c25a8d8e1e6a
Zdaada <3484506583@qq.com>
2023-07-12 22:25:12 +0800

    优化了框架，添加了平滑锚点

---
+++ script.js
@@ -0,0 +1,11 @@
+// 获取 title 标签元素
+let titleElement = document.getElementById("PageTitle");
+
+// 定义一个变量来存储要显示的字符串
+let dynamicTitle = "美好的每一天";
+
+// 将动态标题赋值给 title 标签
+titleElement.innerText = dynamicTitle;
+
+// 获取图片元素
+let bgImage = document.getElementsByClassName("bg");
